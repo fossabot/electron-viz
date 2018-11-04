35a93e586affccf4d09eebe5402b9ff5f3742298
Bruno! <bruno@theBashShell.theBashShell>
2018-11-04 13:10:15 +0000

    fixed

--- viz.js
+++ viz.js
@@ -2,19 +2,22 @@ const csvFilePath = './teams.csv';
 const picasso = require('picasso.js'); 
 const csv = require('csvtojson');
 
+let Data;
 // csv to json 
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{ 
-    let transformed = transform(jsonObj);   
+    let transformed = transform(jsonObj); 
+    Data = transformed;  
     drawPieChart(transformed);
+    drawDonutChart(transformed); 
+
 });
  
 // convert to picasso data format
 let transform = (data) => { 
   let dataArray = [];
   let transformedData = [];
-  dataArray.push(['draws', 'games', 'goalsAgainst', 'goalsFor', 'losses', 'ranking', 'redCards', 'team', 'wins', 'yellowCards']); 
   
   for(let i = 0; i < data.length; i++) { 
     transformedData.push(data[i].draws);
@@ -29,49 +32,189 @@ let transform = (data) => {
     transformedData.push(data[i].yellowCards);
     dataArray.push(transformedData);
     transformedData = [];    
-  }
+  } 
+  dataArray.unshift(['draws', 'games', 'goalsAgainst', 'goalsFor', 'losses', 'ranking', 'redCards', 'team', 'wins', 'yellowCards']); 
+  
   
   return dataArray;
 };
+
+
+let win = document.querySelector("#win");
+win.addEventListener('click', () => {
+  drawPieChart(Data, "wins");  
+});
  
+let lose = document.querySelector("#lose");
+lose.addEventListener('click', () => {
+  drawPieChart(Data, "losses");  
+});
 
-
-// pie chart visualization
-let drawPieChart = (Data) => {
+// pie chart for wins
+let drawPieChart = (Data, field = "wins") => {
   picasso.chart({
-  element: document.querySelector('#pie'),
-  data : [{
-    type: "matrix",
-    data: Data
-  }],
-  settings: {
-    scales: {
-      c: {
-        data: { extract: { field: 'team' } }, type: 'color'
-      }
-    },
-    components: [{
-      type: 'legend-cat',
-      scale: 'c'
-    },{
-      key: 'p',
-      type: 'pie',
-      data: {
-        extract: {
-          field: 'team',
-          props: {
-            num: { field: 'wins' }
-          }
+    element: document.querySelector('#pie'),
+    data: [{
+      type: "matrix",
+      data: Data
+    }],
+    settings: {
+      scales: {
+        c: {
+          data: {
+            extract: {
+              field: 'team'
+            }
+          },
+          type: 'color'
         }
       },
-      settings: {
-        slice: {
-          arc: { ref: 'num' },
-          fill: { scale: 'c' }
+      components: [{
+        type: 'legend-cat',
+        scale: 'c'
+      }, {
+        key: 'p',
+        type: 'pie',
+        data: {
+          extract: {
+            field: 'team',
+            props: {
+              num: {
+                field: field
+              }
+            }
+          }
+        },
+        settings: {
+          slice: {
+            arc: {
+              ref: 'num'
+            },
+            fill: {
+              scale: 'c'
+            }
+          }
         }
-      }
-    }]
-  }
-})
-    
-  };
+      }]
+    },
+    created: () => console.log("I've been created"),
+    beforeRender: () => console.log("Before Render"),
+    beforeMount: () => console.log("Before Mount"),
+    mounted:() => console.log("Mounted")
+
+  });
+
+};
+
+
+
+let drawDonutChart = (Data) => {
+  picasso.chart({
+    element: document.querySelector('#donut'),
+    data: [{
+      type: "matrix",
+      data: Data
+    }],
+    settings: {
+      scales: {
+        c: {
+          data: { extract: { field: 'team' } }, type: 'color'
+        }
+      },
+      components: [{
+        type: 'legend-cat',
+        scale: 'c'
+      }, {
+        key: 'p',
+        type: 'pie',
+        data: {
+          extract: {
+            field: 'team',
+            props: {
+              num: { field: 'wins' }
+            }
+          }
+        },
+        settings: {
+          padAngle: 0.01,
+          slice: {
+            cornerRadius: 2,
+            innerRadius: 0.2,
+            arc: { ref: 'num' },
+            fill: { scale: 'c' } 
+          }
+        }
+      }]
+    }
+  });
+};
+
+
+let drawScatter = (Data) => {
+  picasso.chart({
+    element: document.querySelector('#scatter'),
+    data: [{
+      type: "matrix",
+      data: Data
+    }],
+    settings: {
+      scales: {
+        s: {
+          data: {
+            field: 'yellowCards'
+          },
+          expand: 0.2,
+          invert: true
+        },
+        m: {
+          data: {
+            field: 'team'
+          },
+          expand: 0.1
+        },
+        col: {
+          data: { extract: { field: 'team' } },
+          type: 'color'
+        }
+      },
+      components: [{
+        key: 'y-axis',
+        type: 'axis',
+        scale: 's',
+        dock: 'left'
+      }, {
+        type: 'legend-cat',
+        dock: 'right',
+        scale: 'col'
+      }, {
+        key: 'x-axis',
+        type: 'axis',
+        scale: 'm',
+        dock: 'bottom'
+      }, {
+        key: 'p',
+        type: 'point',
+        data: {
+          extract: {
+            field: 'yellowCards',
+            props: {
+              y: { field: 'yellowCards' },
+              x: { field: 'team' },
+              group: { field: 'yellowCards' }
+            }
+          }
+        },
+        settings: {
+          x: { scale: 'm' },
+          y: { scale: 's' },
+          shape: 'circle',
+          size: () => Math.random(),
+          strokeWidth: 2,
+          stroke: '#fff',
+          opacity: 0.8,
+          fill: { scale: 'col', ref: 'group' }
+        }
+      }]
+    }
+  });
+};
